22c7b5b31acdbbcf2527d7fe7f2e9031b842ccd4
anujmishra1823-boop <anujmishra1823@gmail.com>
2026-09-19 23:18:49 +0530

    Initialize FastAPI setup

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Hello FastAPI"}
+
+@app.get("/about")
+def about():
+    return {"message": "Learning FastAPI"}
