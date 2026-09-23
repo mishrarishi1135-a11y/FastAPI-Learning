85c8a61a456c108500968f230b5a7d74e0c09f93
anujmishra1823-boop <anujmishra1823@gmail.com>
2026-09-23 23:35:35 +0530

    path parametrs

--- Basics_of_fastapi/get_method.py
+++ Basics_of_fastapi/get_method.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 import json
+from pathlib import Path
 
 app = FastAPI()
 
+BASE_DIR = Path(__file__).resolve().parent
+DATA_FILE = BASE_DIR / "patients.json"
+
 
 def load_data():
-    with open('patients.json', 'r') as f:
+    with open(DATA_FILE, 'r') as f:
         data = json.load(f)
 
     return data
@@ -24,3 +28,12 @@ def about():
 def view():
     data = load_data()
     return data
+
+@app.get('/patient/{patient_id}')    # This is our route.
+def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB',example='P001')):    # This is our function.
+
+    # Now write the logic of function,(load all the patient).
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    return {'error':'patient not found'}
